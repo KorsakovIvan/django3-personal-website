924137531e551ed257197a1b3f1ceaf7eecef7b9
ivankorsakov <ivkorsakov@gmail.com>
2022-04-05 20:42:16 +0000

    update templates and settings

--- my_website-project/website/settings.py
+++ my_website-project/website/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kcu!xk)r6ffg#6%t-bgkbljdqw#zh$pfjdy1f5fs&0p9eoa_@s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ivankorsakov.pythonanywhere.com']
 
 
 # Application definition
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
